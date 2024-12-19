fe4d277bd74ce97f7599260bb30b504d59ab2da0
kumar174535 <kumarsanisetty@duck.com>
2024-12-19 13:40:46 +0530

    variable stmt

---
+++ variable.sh
@@ -0,0 +1,6 @@
+#variable - If we assign a name to a set of data is called variable
+# to access variable we use $sign or ${var_name}
+
+learner=kumar
+echo Devops learner - $learner
+echo AWS learner - $learner
