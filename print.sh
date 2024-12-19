6a33b030f1cafcc3a2f277a31ba5dd76463eeedc
kumar174535 <kumarsanisetty@duck.com>
2024-12-19 12:59:58 +0530

    echo color stmt

--- print.sh
+++ print.sh
@@ -1,1 +1,8 @@
 echo Hi kumar
+
+echo -e "\e[31m Hello Kumar\e[0m"
+echo -e "\e[32m Hello kumar\e[0m"
+echo -e "\e[33m Hello kumar\e[0m"
+echo -e "\e[34m Hello kumar\e[0m"
+echo -e "\e[35m hello kumar\e[0m"
+echo -e "\e[36m Hello kumar\e[0m"
