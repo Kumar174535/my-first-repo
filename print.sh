58bc2dd934b01534e4f9b426b9d5619f579cabdc
kumar174535 <kumarsanisetty@duck.com>
2024-12-19 13:05:55 +0530

    echo color stmt modified

--- print.sh
+++ print.sh
@@ -1,8 +1,8 @@
 echo Hi kumar
-
-echo -e "\e[31m \nHello Kumar\e[0m" Red Color
-echo -e "\e[32m Hello kumar\e[0m"
-echo -e "\e[33m Hello kumar\e[0m"
-echo -e "\e[34m Hello kumar\e[0m"
-echo -e "\e[35m hello kumar\e[0m"
-echo -e "\e[36m Hello kumar\e[0m"
+\n
+echo -e "\e[31m Hello Kumar\e[0m" - Red Color
+echo -e "\e[32m Hello kumar\e[0m" - Green Color
+echo -e "\e[33m Hello kumar\e[0m" - yellow Color
+echo -e "\e[34m Hello kumar\e[0m" - Blue Color
+echo -e "\e[35m hello kumar\e[0m" - Magenta Color
+echo -e "\e[36m Hello kumar\e[0m" - Cyan Color
